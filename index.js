1925002d64fb7bd8fd26346f1dad6dd62e00fa6b
rizkysampratama <rizkysampratama@gmail.com>
2019-08-17 20:21:04 +0700

    adding database field Scheme

--- index.js
+++ index.js
@@ -51,16 +51,26 @@ app.get('/contact', (req, res) => {
 })
 
 // * Posts Route
-app.get('/post/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id)
-  console.log(post)
-  res.render('post', {
-    post
-  })
-})
-
 app.get('/post/new', (req, res) => {
   res.render('create')
+})
+
+app.get('/post/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    console.log(post)
+    res.render('post', {
+      post
+    })
+  } catch (error) {
+    console.log(error)
+    if (error.name === 'CastError') {
+      res.status(404).send('Post ID not Found')
+    } else {
+      res.status(500).send('Error Getting Post')
+    }
+  }
+
 })
 
 app.post('/posts/store', (req, res) => {
