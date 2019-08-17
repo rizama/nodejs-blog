692711e961ef81d59c397b9764add61e020f7aaa
rizkysampratama <rizkysampratama@gmail.com>
2019-08-17 16:33:13 +0700

    getting single database documents

--- test.js
+++ test.js
@@ -6,9 +6,14 @@ mongoose.connect('mongodb://localhost/node-js-test-blog', {
   useNewUrlParser: true
 })
 
-Post.find({}, (error, posts) => {
-  console.log(posts)
+// Post.find({}, (error, posts) => {
+//   console.log(posts)
+// })
+
+Post.findById("5d52d848f501c26fb844e5d1", (error, post) => {
+  console.log(error, post)
 })
+
 
 // Post.create({
 //   title: "My Second Blog Title",
