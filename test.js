25fecbbb6c6b67450f63269368e42f79f054e7b6
rizkysampratama <rizkysampratama@gmail.com>
2019-08-17 16:43:47 +0700

    Deleting data fromd database

--- test.js
+++ test.js
@@ -2,15 +2,28 @@ const mongoose = require('mongoose')
 
 const Post = require('./database/models/Post')
 
+// * Konek ke database mongodb
 mongoose.connect('mongodb://localhost/node-js-test-blog', {
   useNewUrlParser: true
 })
 
+// * set False useFindAndModify agar bisa memakai findByIdAndUpdate dll
+mongoose.set('useFindAndModify', false);
+
+// * Ambil semua data pada database 
 // Post.find({}, (error, posts) => {
 //   console.log(posts)
 // })
 
+// * ambil satu data bedasarkan ID
 Post.findById("5d52d848f501c26fb844e5d1", (error, post) => {
+  console.log(error, post)
+})
+
+// * ambil data dan update dari database
+Post.findByIdAndUpdate("5d52d848f501c26fb844e5d1", {
+  title: 'My First Blog Edited Again'
+}, (error, post) => {
   console.log(error, post)
 })
 
