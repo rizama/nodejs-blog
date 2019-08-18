2d4b06917ecbdf1c9ce8197141a96c722ddaa6fb
rizkysampratama <rizkysampratama@gmail.com>
2019-08-18 12:09:01 +0700

    Mongoose model hooks

--- database/models/User.js
+++ database/models/User.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String
+})
+
+UserSchema.pre('save', function (next) {
+  // Get the User
+  const user = this
+
+  // Encrypt the password
+  bcrypt.hash(user.password, 10, function (error, encrypted) {
+    // Change Password unencrypted to encrypted 
+    user.password = encrypted
+
+    next()
+  })
 })
 
 module.exports = mongoose.model('User', UserSchema)
